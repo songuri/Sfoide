d757e4895fe4235325b3182f7384d0530f22f15e
songuri <tngkr8121@naver.com>
2020-08-09 01:46:03 +0900

    map view 기초 다지기

---
+++ app/src/main/java/com/studyfork/sfoide/DetailActivity.kt
@@ -0,0 +1,13 @@
+package com.studyfork.sfoide
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        //https://db-log.tistory.com/entry/KotlinFragment%EC%97%90%EC%84%9C-MapView%EB%A1%9C-GoogleMap%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0
+    }
+}
